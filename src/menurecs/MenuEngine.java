c5668beeb20489ca05c1a9af3f86ea1e07b7d130
Chiang Fong Lee <myself@cflee.net>
2017-04-03 15:15:54 +0800

    Refactor to provide rudimentary API over HTTP
    - Using Spark and Gson, serving on port 8080 and /recommend

--- src/menurecs/MenuEngine.java
+++ src/menurecs/MenuEngine.java
@@ -1,5 +1,8 @@
 package menurecs;
 
+import static spark.Spark.*;
+
+import com.google.gson.Gson;
 import ilog.concert.*;
 import ilog.cplex.IloCplex;
 
@@ -7,39 +10,115 @@ import java.io.IOException;
 import java.util.*;
 
 public class MenuEngine {
+
+    static Map<String, MenuItem> menuItems;
+    static Map<String, List<String>> recommendations;
+
     public static void main(String[] args) {
         // load data
         String dataDir = "data";
         DataFileLoader loader = new DataFileLoader(dataDir);
-        Map<String, String> config;
-        Map<String, MenuItem> menuItems;
-        Map<String, Integer> currents;
-        Map<String, List<String>> recommendations;
-        try {
-            config = loader.readConfig();
 
+        try {
             menuItems = loader.readMenu();
             System.out.println("Read menu items: " + menuItems.size());
 
-            currents = loader.readCurrent();
-            System.out.println("Read current: " + currents.size());
-
             recommendations = loader.readRecommendation();
-            System.out.println("Read recommendation: " + recommendations.size());
+            System.out.println("Read recommendations: " + recommendations.size());
         } catch (IOException e) {
             System.err.println("Error: could not read one or more files.");
             e.printStackTrace();
             return;
         }
 
+        // config
+        port(8080);
+        // provide routes and logic
+        get("/recommend", (request, response) -> {
+            // check for required params
+            String[] requiredParams = {"customerid", "outputlength", "numpax", "targetspend", "itemids", "itemqty"};
+            List<String> missingParams = new ArrayList<>();
+            for (String param : requiredParams) {
+                if (request.queryParams(param) == null) {
+                    missingParams.add(param);
+                }
+            }
+            if (!missingParams.isEmpty()) {
+                response.status(400);
+                return "Missing parameter(s): " + String.join(", ", missingParams);
+            }
+
+            // check for integer params
+            String[] integerParams = {"customerid", "outputlength", "numpax"};
+            List<String> notIntegerParams = new ArrayList<>();
+            for (String param : integerParams) {
+                try {
+                    Integer.parseInt(request.queryParams(param));
+                } catch (NumberFormatException e) {
+                    notIntegerParams.add(param);
+                }
+            }
+            if (!notIntegerParams.isEmpty()) {
+                response.status(400);
+                return "Non-integer parameter(s): " + String.join(", ", notIntegerParams);
+            }
+
+            // check for double params
+            String[] doubleParams = {"customerid", "outputlength", "numpax"};
+            List<String> notDoubleParams = new ArrayList<>();
+            for (String param : doubleParams) {
+                try {
+                    Double.parseDouble(request.queryParams(param));
+                } catch (NumberFormatException e) {
+                    notDoubleParams.add(param);
+                }
+            }
+            if (!notDoubleParams.isEmpty()) {
+                response.status(400);
+                return "Non-double parameter(s): " + String.join(", ", notDoubleParams);
+            }
+
+            // all parameters are validated somewhat
+            try {
+                String customerId = request.queryParams("customerid");
+                int outputLength = Integer.parseInt(request.queryParams("outputlength"));
+                int numPax = Integer.parseInt(request.queryParams("numpax"));
+                double targetSpend = Double.parseDouble(request.queryParams("targetspend"));
+
+                Map<String, Integer> currentOrder = new HashMap<>();
+                String[] itemIdsStrings = request.queryParams("itemids").split(",");
+                String[] itemQtyStrings = request.queryParams("itemqty").split(",");
+                if (itemIdsStrings.length != itemQtyStrings.length) {
+                    throw new Exception("Differing number of item ids and qty");
+                }
+                for (int i = 0; i < itemIdsStrings.length; i++) {
+                    int itemQty = Integer.parseInt(itemQtyStrings[i]);
+                    currentOrder.put(itemIdsStrings[i], itemQty);
+                }
+
+                List<String> results = computeRecommendation(customerId, outputLength, numPax, targetSpend, currentOrder);
+
+                return new Gson().toJson(results);
+            } catch (Exception e) {
+                e.printStackTrace();
+                response.status(400);
+                return "Some exception occurred: " + e.getClass() + ": " + e.getMessage();
+            }
+        });
+    }
+
+    public static List<String> computeRecommendation(String customerId, int outputLength, int numPax, double spendPerPax,
+                                             Map<String, Integer> currents) {
         // process data: currently ordered items
         double curTotalPrice = 0.0;
         for (Map.Entry<String, Integer> current : currents.entrySet()) {
+            System.out.print(menuItems);
+            System.out.print(menuItems.get(current.getKey()));
             curTotalPrice += menuItems.get(current.getKey()).getPrice();
         }
 
         // process data: only get the current customer
-        List<String> customerRecommendations = recommendations.get(config.get("customerId"));
+        List<String> customerRecommendations = recommendations.get(customerId);
 
         // process data: prices and categories
         double[] itemPrices = new double[customerRecommendations.size()];
@@ -60,6 +139,9 @@ public class MenuEngine {
 
             itemCategories[i] = categoryIndex;
         }
+
+        // result list
+        List<String> results = new ArrayList<>();
 
         // init model
         try {
@@ -101,11 +183,11 @@ public class MenuEngine {
 
             // number of things to output
             // outputLength == sum(xs)
-            cplex.addEq(Integer.parseInt(config.get("outputLength")), cplex.sum(xs), "outputLength");
+            cplex.addEq(outputLength, cplex.sum(xs), "outputLength");
 
             // totalPrices <= (1 + Z) * (NS - amount spent)
             IloLinearNumExpr totalPrices = cplex.scalProd(itemPrices, xs);
-            double budget = Integer.parseInt(config.get("numOfPax")) * Double.parseDouble(config.get("spendPerPax"));
+            double budget = numPax * spendPerPax;
             double remainingBudget = budget - curTotalPrice;
             System.out.println("Budget: " + budget);
             System.out.println("Remaining budget: " + remainingBudget);
@@ -143,6 +225,7 @@ public class MenuEngine {
                         MenuItem item = menuItems.get(customerRecommendations.get(i));
                         System.out.println(item.getId() + "," + item.getDescription() + "," + item.getCategory()
                                 + "," + item.getPrice());
+                        results.add(item.getId());
                     }
                 }
             } else {
@@ -160,5 +243,7 @@ public class MenuEngine {
             System.err.println("Error: could not find CPLEX shared library.");
             e.printStackTrace();
         }
+
+        return results;
     }
 }
